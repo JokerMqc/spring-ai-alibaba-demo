aff520b6000826f940ec7184153cc9580a22daf9
maoqichuan <981848257@qq.com>
2026-06-17 22:35:38 +0800

    feat:初始化项目结构

---
+++ src/main/java/com/example/demo/SpringAiAlibabaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAlibabaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiAlibabaDemoApplication.class, args);
+    }
+}
